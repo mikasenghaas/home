76efbd88b2eae48c2b516872397c3d43e1b5c8ce
jonas-mika <accounts@jonassenghaas.de>
2022-08-24 15:26:40 +0200

    auth for admin page

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,18 +1,44 @@
 // Admin.tsx
 // By: Mika Senghaas
 // custom styles
-import * as md from '../styles/MarkdownStyles'
+import { useState, useEffect } from "react";
+import * as md from "../styles/MarkdownStyles";
 
 // custom components
-import PageBox from '../components/PageBox'
+import PageBox from "../components/PageBox";
 
 const Admin = () => {
-  return (
-    <PageBox>
-      <md.H1>Admin</md.H1>
-      <md.Divider />
-    </PageBox>
-  )
-}
+  const [pw, setPw] = useState<string>("test");
+
+  /*
+  useEffect(() => {
+    const p = prompt("Password");
+    setPw(p!);
+  }, []);
+  */
+
+  if (pw === "test") {
+    return (
+      <PageBox>
+        <md.H1>Admin</md.H1>
+        <md.Divider />
+        <md.P>
+          Welcome to the <md.InlineCode>Admin</md.InlineCode> page. Create, edit and remove material from here.
+        </md.P>
+        <md.Divider />
+        <md.P>
+          Go back to the<md.Link> HomePage</md.Link>
+        </md.P>
+      </PageBox>
+    );
+  } else {
+    return (
+      <PageBox>
+        <md.H1>Unauthorised</md.H1>
+        <md.Divider />
+      </PageBox>
+    );
+  }
+};
 
 export default Admin;
