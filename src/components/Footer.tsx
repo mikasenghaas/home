466456b5710cc403b74639ec6d6badfa58eeb3e7
jonas-mika <accounts@jonassenghaas.de>
2022-08-26 18:29:31 +0200

    [fix] added proper padding top for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ import AdminToggle from '../components/AdminToggle'
 
 const Footer = (props: any) => {
   return (
-    <Flex direction='column' justifyContent='center' alignItems='center' h='30px' marginTop='auto' py='2rem'>
+    <Flex direction='column' justifyContent='center' alignItems='center' h='30px' marginTop='auto' pb='2rem' pt='5rem'>
       <AdminToggle state={props.state} setState={props.setState} />
       <Text mt='.2rem' fontSize='12px'>&copy; Mika Senghaas, 2022</Text>
     </Flex>
