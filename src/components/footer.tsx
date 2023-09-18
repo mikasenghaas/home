93c431bb66822ac7822ecceb5eac800f349761ee
Mika Senghaas <accounts@jonassenghaas.de>
2023-09-18 17:13:18 +0200

    feat(footer): remove tooltip over social link button

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -20,58 +20,53 @@ export function Footer() {
       <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
         <span className="text-sm text-muted-foreground sm:text-base 2xl:text-xl">
           Reach me{" "}
-          <Tooltip>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <TooltipTrigger asChild>
-                  <Button
-                    className="m-0 p-0 font-medium text-accent-foreground"
-                    variant="link"
-                  >
-                    @mikasenghaas
-                  </Button>
-                </TooltipTrigger>
-              </DropdownMenuTrigger>
-              <TooltipContent>Give me a ping</TooltipContent>
-              <DropdownMenuContent>
-                <DropdownMenuItem
-                  onSelect={() =>
-                    window.open("https://github.com/mikasenghaas", "_blank")
-                  }
-                >
-                  <Github className="mr-2 h-4 w-4" />
-                  <span>GitHub</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                  onSelect={() =>
-                    window.open("https://linkedin.com/in/mikasenghaas")
-                  }
-                >
-                  <Linkedin className="mr-2 h-4 w-4" />
-                  <span>LinkedIn</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                  onSelect={() =>
-                    window.open("https://instagram.com/mikasenghaas", "_blank")
-                  }
-                >
-                  <Instagram className="mr-2 h-4 w-4" />
-                  <span>Instagram</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                  onSelect={() =>
-                    window.open(
-                      "https://open.spotify.com/user/1atznoygdksoddgocmbl4buli?si=17e9e23d34ab4aad",
-                      "_blank",
-                    )
-                  }
-                >
-                  <Music className="mr-2 h-4 w-4" />
-                  <span>Spotify</span>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </Tooltip>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                className="m-0 p-0 font-medium text-accent-foreground"
+                variant="link"
+              >
+                @mikasenghaas
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem
+                onSelect={() =>
+                  window.open("https://github.com/mikasenghaas", "_blank")
+                }
+              >
+                <Github className="mr-2 h-4 w-4" />
+                <span>GitHub</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onSelect={() =>
+                  window.open("https://linkedin.com/in/mikasenghaas")
+                }
+              >
+                <Linkedin className="mr-2 h-4 w-4" />
+                <span>LinkedIn</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onSelect={() =>
+                  window.open("https://instagram.com/mikasenghaas", "_blank")
+                }
+              >
+                <Instagram className="mr-2 h-4 w-4" />
+                <span>Instagram</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onSelect={() =>
+                  window.open(
+                    "https://open.spotify.com/user/1atznoygdksoddgocmbl4buli?si=17e9e23d34ab4aad",
+                    "_blank",
+                  )
+                }
+              >
+                <Music className="mr-2 h-4 w-4" />
+                <span>Spotify</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </span>
         <p className="text-xs text-muted-foreground sm:text-sm 2xl:text-base">
           &copy; Mika Senghaas {moment().format("YYYY")}
