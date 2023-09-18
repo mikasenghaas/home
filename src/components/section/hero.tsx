1803298a9c1a8bbc90faa103a4e694a2424a9e1e
Mika Senghaas <accounts@jonassenghaas.de>
2023-09-18 22:39:11 +0200

    feat(hero): added facts and randomise after first message

--- src/components/section/hero.tsx
+++ src/components/section/hero.tsx
@@ -9,6 +9,22 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { useTypewriter, useTypewriters } from "@/hooks/use-typewriter";
+import { shuffle } from "@/lib/utils";
+
+const facts = [
+  "I love my morning coffee ritual.",
+  ...shuffle([
+    "AI research fascinates me.",
+    "I ran the CPH Marathon 2023.",
+    "I listen to UK hip-hop 74% of the time.",
+    "I like cheffing it up from time to time.",
+    "I dream about opening a café.",
+    "I like getting out of my comfort zone.",
+    "I am curious by nature.",
+    "I used to play football for FCSP.",
+    "you can find me in the mountains on Sundays.",
+  ]),
+];
 
 function HeroText() {
   const { message: startMessage } = useTypewriter({
@@ -16,16 +32,9 @@ function HeroText() {
     initialDelay: 1000,
     typing_interval_min: 95,
   });
+
   const { message: fact, selectedMessage: selectedFact } = useTypewriters({
-    messages: [
-      "I love my morning coffee ritual.",
-      "AI research fascinates me.",
-      "I ran the CPH Marathon 2023.",
-      "I listen to UK hip-hop 74% of the time.",
-      "I used to play football for FCSP.",
-      "find me in the mountains on Sundays.",
-      "",
-    ],
+    messages: facts,
     initial_delay: 2000,
   });
 
