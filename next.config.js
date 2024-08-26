235c1b9c7a2a30dc22ac692742c4ec596591ef8b
Mika Senghaas <mail@mikasenghaas.de>
2024-08-26 13:47:03 +0200

    Updated project deps

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  transpilePackages: ['next-mdx-remote']
+};
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
