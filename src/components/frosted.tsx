e93a54bdb4bb9261e6904f57cbfe69b276f7bed6
Mika Senghaas <mail@mikasenghaas.de>
2024-08-28 14:27:14 +0200

    Changed default opacity

--- src/components/frosted.tsx
+++ src/components/frosted.tsx
@@ -43,7 +43,7 @@ export function Frosted({ children, className, ...props }: FrostedProps) {
     <div
       ref={ref}
       className={cn(
-        "relative rounded-xl border p-6 before:pointer-events-none before:absolute before:left-0 before:top-0 before:-z-50 before:h-full before:w-full before:rounded-xl before:bg-[radial-gradient(circle_at_var(--x,_0px)_var(--y,_0px),hsl(var(--accent-foreground)),_transparent)] before:opacity-30",
+        "relative rounded-xl border p-8 before:pointer-events-none before:absolute before:left-0 before:top-0 before:-z-50 before:h-full before:w-full before:rounded-xl before:bg-[radial-gradient(circle_at_var(--x,_0px)_var(--y,_0px),hsl(var(--accent-foreground)),_transparent)] before:opacity-20",
         className,
       )}
       {...props}
