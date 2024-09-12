2b93cafe1421977c8c9264911968c1d18491dd81
Mika Senghaas <mail@mikasenghaas.de>
2024-09-12 10:49:16 +0200

    Remove unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Featured } from "@/components/section/featured";
 import { Hero } from "@/components/section/hero";
 import { Project } from "@/components/section/projects";
-import { Teaching } from "@/components/section/teaching";
 import { Research } from "@/components/section/research";
 import { TLDR } from "@/components/section/tldr";
 
