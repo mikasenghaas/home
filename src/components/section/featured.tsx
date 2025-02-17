672b92a84dcadac2b7c6f3d3a00391bc699c5b24
Mika Senghaas <mail@mikasenghaas.de>
2025-02-17 20:15:27 +0100

    Update featured section

--- src/components/section/featured.tsx
+++ src/components/section/featured.tsx
@@ -7,28 +7,25 @@ import Image from "next/image";
 export function Featured() {
   return (
     <Section className="mt-12">
-      <Frosted className="flex flex-col sm:flex-row gap-8 p-4 sm:p-8">
+      <Frosted className="flex flex-col sm:flex-row gap-8 p-4 sm:p-8 min-h-[350px]">
         <div className="flex-1">
           <p className="text-sm text-accent-foreground">Featured</p>
-          <Link href="/project/addiscoder">
-            <h2 className="text-2xl sm:text-3xl mt-2">I spent my summer in Addis Ababa, Ethiopia 🇪🇹</h2>
-          </Link>
+          <h2 className="text-2xl sm:text-3xl mt-2">I am in San Francisco, CA 🇺🇸</h2>
           <p className="mt-2 text-lg leading-snug 2xl:mt-6 2xl:text-2xl">
-              I was part of the amazing team that ran <span
-              className="text-foreground">AddisCoder 2024</span>. Read more about my
-              journey of a month in Ethiopia, where we taught the country&apos;s
-              most talented high schoolers write their first lines of code in Week 1
-              and solve complex dynamic programming problems in Week 4.
-            </p>
+              I have joined the amazing team at{" "}
+              <a href="https://www.primeintellect.ai/" target="_blank" rel="noopener noreferrer">Prime Intellect</a> as a research scientist intern to make decentralized AI a reality. If you are in SF and would like to connect, let&apos;s grab a coffee, go for a run, or hack away. 
+          </p>
         </div>
-        <div className="flex-1 relative aspect-video">
-          <Image
-            src="/img/addiscoder1.jpeg"
-            alt="AddisCoder"
-            className="object-cover rounded-lg"
-            fill
-          />
-        </div>
+          <div className="flex-1 relative aspect-video">
+            <Link href="https://x.com/PrimeIntellect/status/1879916015813009484" target="_blank" rel="noopener noreferrer">
+              <Image
+                src="/img/prime.jpeg"
+                alt="Prime Intellect"
+                className="object-cover rounded-lg"
+                fill
+              />
+            </Link>
+          </div>
       </Frosted>
     </Section>
   );
